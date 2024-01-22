4aaa668d89d40f935a5bb507aac7749c775f95e8
Dmitry <89468766+Dimaland16@users.noreply.github.com>
2024-01-22 09:56:20 +0300

    Update MyLinkedList.java

--- src/by/it/group251004/kalitsko/lesson10/MyLinkedList.java
+++ src/by/it/group251004/kalitsko/lesson10/MyLinkedList.java
@@ -1,5 +1,3 @@
-package by.it.group251004.kalitsko.lesson10;
-
 import java.util.Collection;
 import java.util.Deque;
 import java.util.Iterator;
@@ -38,27 +36,6 @@ public class MyLinkedList<E> implements Deque<E> {
     public void addLast(E e) {
         add(e);
     }
-
-    @Override
-    public boolean offerFirst(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean offerLast(E e) {
-        return false;
-    }
-
-    @Override
-    public E removeFirst() {
-        return null;
-    }
-
-    @Override
-    public E removeLast() {
-        return null;
-    }
-
     @Override
     public E pollFirst() {
         return poll();
@@ -93,26 +70,6 @@ public class MyLinkedList<E> implements Deque<E> {
     }
 
     @Override
-    public E peekFirst() {
-        return null;
-    }
-
-    @Override
-    public E peekLast() {
-        return null;
-    }
-
-    @Override
-    public boolean removeFirstOccurrence(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean removeLastOccurrence(Object o) {
-        return false;
-    }
-
-    @Override
     public boolean add(E e) {
         Node newNode = new Node(e);
 
@@ -126,16 +83,6 @@ public class MyLinkedList<E> implements Deque<E> {
 
         size++;
         return true;
-    }
-
-    @Override
-    public boolean offer(E e) {
-        return false;
-    }
-
-    @Override
-    public E remove() {
-        return null;
     }
 
     @Override
@@ -160,42 +107,7 @@ public class MyLinkedList<E> implements Deque<E> {
     public E element() {
         return head.data;
     }
-
-    @Override
-    public E peek() {
-        return null;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public void push(E e) {
-
-    }
-
-    @Override
-    public E pop() {
-        return null;
-    }
-
+    
     @Override
     public boolean remove(Object o) {
         if (head == null)
@@ -241,28 +153,8 @@ public class MyLinkedList<E> implements Deque<E> {
     }
 
     @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
     public int size() {
         return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return null;
     }
 
     public String toString(){
@@ -282,18 +174,4 @@ public class MyLinkedList<E> implements Deque<E> {
         return string.toString();
     }
 
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public Iterator<E> descendingIterator() {
-        return null;
-    }
 }
